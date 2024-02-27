af22edd106ea933ca4ccacd0afb652f2ca141a46
Lucas Fraggioli <fraggioli.lucas@gmail.com>
2024-02-27 18:46:16 -0300

    form popover

--- hooks/use-action.ts
+++ hooks/use-action.ts
@@ -32,9 +32,8 @@ export const useAction = <TInput, TOutput>(
         if (!result) {
           return;
         }
-        if (result.fieldErrors) {
-          setFieldErrors(result.fieldErrors);
-        }
+        setFieldErrors(result.fieldErrors);
+
         if (result.error) {
           setError(result.error);
           if (options.onError) {
